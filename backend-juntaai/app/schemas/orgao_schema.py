a9453ee4acb8562e58d842cf66801263aceae80e
samyrafariass <104604760+samyrafariass@users.noreply.github.com>
2025-11-02 16:11:21 -0300

    atualizando...

--- backend-juntaai/app/schemas/orgao_schema.py
+++ backend-juntaai/app/schemas/orgao_schema.py
@@ -1,25 +1,20 @@
 from pydantic import BaseModel
 from typing import Optional
-from datetime import datetime
-from shared import EnderecoBase
 
-class CreateOrgao:
+class CreateOrgao(BaseModel):
     Nome: str
     Descricao_Alerta: str
     Id_Classificacao: int
     Solicitacao: bool
 
-class UpdateOrgao:
+class UpdateOrgao(BaseModel):
     Id_Orgao: int
     Nome: Optional[str]
     Descricao_Alerta: Optional[str]
     Id_Classificacao: Optional[int]
     Solicitacao: Optional[bool]
 
-class AlertaRecebido:
-    Id_Alerta: int
-
-class ResponseOrgao:
+class ResponseOrgao(BaseModel):
     Id_Orgao: int
     Nome: str
     Descricao_Alerta: str
